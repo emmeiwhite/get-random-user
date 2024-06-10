09e0076149e2f49e4673d13d8df4eaff3402b61e
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-10 14:37:16 +0530

    showUser ---> calls getUser() to get user data from the API

--- app.js
+++ app.js
@@ -9,14 +9,47 @@ const btn = get('.btn')
 
 const btns = [...document.querySelectorAll('.icon')]
 
-const fetchPerson = async function () {
-  const response = await fetch(url)
-  console.log(response)
+/** --- Responsible for data fetch and returning an object | Since getUser is an async function, it returns a promise  --- */
+const getUser = async function () {
+  try {
+    const response = await fetch(url)
+
+    const data = await response.json()
+    // let's de-structure the data related to the person
+    const person = data.results[0]
+
+    console.log(person)
+    const { email, phone } = person
+    const { large: image } = person.picture
+    const { password } = person.login
+    const { first, last } = person.name
+    const {
+      dob: { age }
+    } = person
+    const {
+      street: { number, name }
+    } = person.location
+
+    /** --- We return our Object --- */
+    return {
+      email,
+      phone,
+      image,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-async function show() {
-  console.log('showUser Invoked!!!')
-  const person = await fetchPerson()
+const showUser = async () => {
+  // get user from api
+  const person = await getUser()
+
+  // display user
 }
 
 // load user api data on DOMContentLoaded and for the click of each Random button
