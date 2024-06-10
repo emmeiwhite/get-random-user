dc19a331fe91ad238cec8bcd74cc3b9edd0e21d4
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-10 14:00:46 +0530

    Decided to use plain old fetch() for data fetching for this small project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import get from './utils/getElement'
+import get from './utils/getElement.js'
+
 const url = 'https://randomuser.me/api/'
 
 const image = get('.user-img')
@@ -7,3 +8,17 @@ const value = get('.user-value')
 const btn = get('.btn')
 
 const btns = [...document.querySelectorAll('.icon')]
+
+const fetchPerson = async function () {
+  const response = await fetch(url)
+  console.log(response)
+}
+
+async function show() {
+  console.log('showUser Invoked!!!')
+  const person = await fetchPerson()
+}
+
+// load user api data on DOMContentLoaded and for the click of each Random button
+window.addEventListener('DOMContentLoaded', showUser)
+btn.addEventListener('click', showUser)
--- utils/getElement.js
+++ utils/getElement.js
@@ -1,6 +1,8 @@
-export default function getElement(selector) {
+function getElement(selector) {
   const element = document.querySelector(selector)
   if (element) return element
 
   throw new Error('no element selector')
 }
+
+export default getElement
